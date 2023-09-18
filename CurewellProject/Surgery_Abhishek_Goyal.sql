624b60a0df9086eb5c6c66ce3fb23144c6d5de38
Akshita44 <83125396+Akshita44@users.noreply.github.com>
2023-09-18 17:37:34 +0530

    Update Surgery_Abhishek_Goyal.sql

--- CurewellProject/Surgery_Abhishek_Goyal.sql
+++ CurewellProject/Surgery_Abhishek_Goyal.sql
@@ -1,8 +1,8 @@
 create table Surgery(
 SurgeryId int identity(5000,1) constraint pk_Surgery_SurgeryId primary Key,
-DoctorId int constraint fk_Surgery_DoctorId foreign Key references Doctor(DoctorId),
+DoctorId int constraint fk_Surgery_Doctor_DoctorId foreign Key references Doctor(DoctorId),
 SurgeryDate Date not null,
 StartTime Decimal(4,2) not null,
 EndTime Decimal(4,2) not null,
-SurgeryCategory char(3) constraint fk_Surgery_SurgeryCategory foreign Key references Specialization(SpecializationCode)
+SurgeryCategory char(3) constraint fk_Surgery_Specialization_SurgeryCategory foreign Key references Specialization(SpecializationCode)
 )
