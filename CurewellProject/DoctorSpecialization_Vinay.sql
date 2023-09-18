b051f73403cde633f0109da7db5c2998cfb73331
Akshita44 <83125396+Akshita44@users.noreply.github.com>
2023-09-18 17:35:28 +0530

    Update DoctorSpecialization_Vinay.sql

--- CurewellProject/DoctorSpecialization_Vinay.sql
+++ CurewellProject/DoctorSpecialization_Vinay.sql
@@ -1,8 +1,8 @@
 CREATE TABLE DoctorSpecialization(
-	DoctorId INT CONSTRAINT fk_Doctor_DoctorId_DoctorId foreign Key references Doctor(DoctorId),
-	SpecializationCode Char(3) CONSTRAINT fk_Specialization_SpecializationCode_SpecializationCode foreign Key references Specialization(SpecializationCode),
+	DoctorId INT CONSTRAINT fk_DoctorSpecialization_Doctor_DoctorId foreign Key references Doctor(DoctorId),
+	SpecializationCode Char(3) CONSTRAINT fk_DoctorSpecialization_Specialization_SpecializationCode foreign Key references Specialization(SpecializationCode),
 	SpecializationDate DATE NOT NULL,
-	Constraint ck_DoctorId_SpecializationCode PRIMARY KEY (DoctorId,SpecializationCode)
+	Constraint ck_DoctorSpecialization_DoctorId_SpecializationCode PRIMARY KEY (DoctorId,SpecializationCode)
 )
 INSERT INTO DoctorSpecialization(DoctorId,SpecializationCode,SpecializationDate) Values(1001,'ANE','2010-01-01'),(1002,'CAR','2010-01-01'),(1003,'CAR','2010-01-01')
 Select * from DoctorSpecialization
